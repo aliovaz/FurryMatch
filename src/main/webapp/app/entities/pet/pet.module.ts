e9ed364bc97e5e0bbd2e4ef9a0a02edbd5896a75
Alina Zeledón <alinazeledon@tactdata.com>
2023-03-29 16:31:22 -0600

    Revert "Merge branch 'dev' into alina"
    This reverts commit e4c8a8359e14a289944b7be899ee1e8fa64fbc18, reversing
    changes made to dd3d7270541b33852365b3d4dcb0072dba73b9c7.

--- src/main/webapp/app/entities/pet/pet.module.ts
+++ src/main/webapp/app/entities/pet/pet.module.ts
@@ -5,10 +5,9 @@ import { PetDetailComponent } from './detail/pet-detail.component';
 import { PetUpdateComponent } from './update/pet-update.component';
 import { PetDeleteDialogComponent } from './delete/pet-delete-dialog.component';
 import { PetRoutingModule } from './route/pet-routing.module';
-import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @NgModule({
-  imports: [SharedModule, PetRoutingModule, NgxDropzoneModule],
+  imports: [SharedModule, PetRoutingModule],
   declarations: [PetComponent, PetDetailComponent, PetUpdateComponent, PetDeleteDialogComponent],
 })
 export class PetModule {}
